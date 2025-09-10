672ed417406eb5c36cee5b98ad20b230de831521
xiebin <xiebin@orvibo.com>
2025-09-10 19:38:05 +0800

    chore(webrtc-react): 集成vconsole

--- webrtc-react/src/main.jsx
+++ webrtc-react/src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import VConsole from 'vconsole';
 import '@ant-design/v5-patch-for-react-19';
 import 'antd/dist/reset.css';
 import './styles/css/styles.scss';
 import './index.css';
 import App from './App.jsx';
+
+new VConsole();
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
